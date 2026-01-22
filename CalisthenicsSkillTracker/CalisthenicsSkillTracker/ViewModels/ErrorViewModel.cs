660823f0e884eca11202827e4123b9718d2e1420
Alexander Cvetkov <alexandercvetkov0@gmail.com>
2026-01-22 18:16:53 +0200

    Initial project setup.

---
+++ CalisthenicsSkillTracker/CalisthenicsSkillTracker/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CalisthenicsSkillTracker.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
