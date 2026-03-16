c41b91367c2c41da5869bf4658365a93634d1b90
Alexander Cvetkov <alexandercvetkov0@gmail.com>
2026-03-16 21:28:03 +0200

    Deleted old migrations and recreated initial migration.

--- CalisthenicsSkillTracker/CalisthenicsSkillTracker.Data.Models/ApplicationUser.cs
+++ CalisthenicsSkillTracker/CalisthenicsSkillTracker.Data.Models/ApplicationUser.cs
@@ -6,4 +6,7 @@ public class ApplicationUser : IdentityUser
 {
     public virtual ICollection<SkillProgress> SkillProgressRecords { get; set; }
         = new List<SkillProgress>();
+
+    public virtual ICollection<Workout> Workouts { get; set; }
+        = new List<Workout>();
 }
