21e6bfa5dac607a3fc928d754772b378b5a4c8a8
Alexander Cvetkov <alexandercvetkov0@gmail.com>
2026-03-17 17:40:12 +0200

    Implementation of exception entity and output messages.

---
+++ CalisthenicsSkillTracker/CalisthenicsSkillTracker.GCommon/Exceptions/EntityCreatePersistException.cs
@@ -0,0 +1,15 @@
+﻿namespace CalisthenicsSkillTracker.GCommon.Exceptions;
+
+public class EntityCreatePersistException : Exception
+{
+    public EntityCreatePersistException()
+    {
+        
+    }
+
+    public EntityCreatePersistException(string message)
+        : base(message)
+    {
+        
+    }
+}
---
+++ CalisthenicsSkillTracker/CalisthenicsSkillTracker.GCommon/OutputMessages.cs
@@ -0,0 +1,13 @@
+﻿namespace CalisthenicsSkillTracker.GCommon;
+
+public static class OutputMessages
+{
+    public static class Workout
+    {
+        public const string WorkoutSaveError = "An error occurred while creating the workout. Please try again in a few minutes.";
+
+        public const string WorkoutExerciseSaveError = "An error occurred while creating the workout exercise. Please try again in a few minutes.";
+
+        public const string WorkoutSetSaveError = "An error occurred while creating the workout set. Please try again in a few minutes.";
+    }
+}
