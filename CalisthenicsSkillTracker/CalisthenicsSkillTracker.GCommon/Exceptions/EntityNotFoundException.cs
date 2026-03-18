1622d6a4feb6a0b54433630a72754b73d713195b
Alexander Cvetkov <alexandercvetkov0@gmail.com>
2026-03-18 18:29:51 +0200

    Implementation of new exceptions.

---
+++ CalisthenicsSkillTracker/CalisthenicsSkillTracker.GCommon/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,15 @@
+﻿namespace CalisthenicsSkillTracker.GCommon.Exceptions;
+
+public class EntityNotFoundException : Exception
+{
+    public EntityNotFoundException()
+    {
+        
+    }
+
+    public EntityNotFoundException(string message)
+        : base(message)
+    {
+        
+    }
+}
